5d8d37f105b7fbe756c318b5d528e75f675ec9bb
Dominic Chen <dominic.chen630@gmail.com>
2023-04-29 17:47:38 -0400

    implemented hand detection
    - implemented hand detection for webcam using openCV, mediapipe, and cvzone

--- src/camera.py
+++ src/camera.py
@@ -1,12 +1,31 @@
-import av
+import cv2
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+import mediapipe as mp
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, WebRtcMode
+from cvzone.HandTrackingModule import HandDetector
+
+detector = HandDetector(maxHands=1)
+
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        # Convert the frame to RGB
+        img = cv2.cvtColor(frame.to_ndarray(format="bgr24"), cv2.COLOR_BGR2RGB)
+
+        # Find hands in the frame
+        hands, img = detector.findHands(img)
+
+        # Convert the image back to BGR
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        return img
 
 def camera_recognition():
+
     webrtc_streamer(
         key="WYH",
-        video_processor_factory=None,
+        mode=WebRtcMode.SENDRECV,
+        video_transformer_factory=VideoTransformer,
         async_transform=False,
         media_stream_constraints={"video": True, "audio": False},
-        async_processing=False,
+        async_processing=True,
     )
