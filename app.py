6b0283d489b3fa53e5a0132ac973454611dc4809
Andre <dtandre331@gmail.com>
2023-04-29 01:07:31 -0400

    Implemented query and dropdown button
    - implemented dropdown button
    - allowed any language to be the code
    - sends out the translated text

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from googletrans import LANGUAGES
+from src.translate import translate_lang
 
 st.set_page_config(
     page_title="Translating App",
@@ -12,10 +14,24 @@ st.set_page_config(
     }
 )
 
-st.write("""
-# Hello *World! yes*""")
+st.markdown(
+    f'<link rel="stylesheet" href="/styles.css">',
+    unsafe_allow_html=True,
+)
 
-st.write("""
-# WE ARE GOING TO WIN THIS HACKAHTON **YES**""")
+languages = {v: k for k, v in LANGUAGES.items()}
 
-st.image('./hasbulla 2.jpeg')
+lang_names = sorted(list(languages.keys()))
+
+target_lang = st.selectbox("Select target language", options=lang_names)
+
+label = 'Enter text'
+
+user_input = ""
+user_input = st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder="enter text", disabled=False, label_visibility="visible")
+
+if not user_input == "":
+    query = user_input
+    language = languages.get(target_lang)
+    response = translate_lang(query, language)
+    st.text_input(label=f"Output in {target_lang}", value=f"{response}", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder="", disabled=True, label_visibility="visible")
--- src/translate.py
+++ src/translate.py
@@ -1,31 +1,14 @@
-import openai
 import cohere
-from api import OPENAI_KEY, COHERE_KEY
-
-openai.api_key = OPENAI_KEY
+from googletrans import Translator
+from src.api import COHERE_KEY
 
 cohere_client = cohere.Client(COHERE_KEY)
 
-def translate(text, target, choice="translate"):
+def translate_lang(text, target):
     response = cohere_client.detect_language(texts=[text])
     lang = response.results[0]
+    
     if lang != target:
-        if choice == "translate":
-            prompt = f"Use google translate and output this text: '{text}' from {lang} to {target}"
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5
-        )
-        text = response.choices[0].text.strip()
+        result = Translator().translate(text, dest=target)
+        text = result.text
     return text
-
-while True:
-    query = input("User query: ")
-    if query == "e":
-        break
-    language = input("User language: ")
-    print(translate(query, language))
